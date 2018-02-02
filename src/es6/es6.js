a9580a68df7a46324d479ba08b33be1c4a22bd15
__Steel笑口 <kewangxunqiu@163.com>
2018-02-02 18:07:49 +0800

    class-study

--- src/es6/es6.js
+++ src/es6/es6.js
@@ -2,56 +2,59 @@ import './es6.css';
 
 // es6 学习
 import { letConst } from './section/let&const'; // let&const
-letConst();
 
 import { destructuring } from './section/destructuring'; // destructuring
-destructuring();
 
 import { stringExtend } from './section/stringExtend'; // stringExtend
-stringExtend();
 
 import { regExp } from './section/regExp'; // regExp
-regExp();
 
 import { numberExtend } from './section/numberExtend'; // arrayExtend
-numberExtend();
 
 import { functionExtend } from './section/functionExtend'; // functionExtend
-functionExtend();
 
 import { arrayExtend } from './section/arrayExtend'; // arrayExtend
-arrayExtend();
 
 import { objectExtend } from './section/objectExtend'; // objectExtend
-objectExtend();
 
 import { symbol } from './section/symbol'; // symbol
-symbol();
 
 import { setMap } from './section/set&map'; // set&map
-setMap();
 
 import { proxy } from './section/proxy'; // proxy
-proxy();
 
 import { reflect as reflectAlias } from './section/reflect'; // reflect
-reflectAlias();
 
 import promise from './section/promise'; // promise
-promise();
 
 import { iterator } from './section/iterator'; // iterator & for...of
-iterator();
 
 import classStudy from './section/class'; // class
-classStudy();
 
 
 // import 'prettify';
 // import 'prettify-css'; 在webpack中alias，会被打包到es6.js中，另一种方法是直接在html中引入外部的js和css
 // 代码美化
 $(window).load(function() {
+
     $('pre').addClass('prettyprint linenums');
     window.prettyPrint();
     $('pre').show();
+
+    letConst();
+    destructuring();
+    stringExtend();
+    regExp();
+    numberExtend();
+    functionExtend();
+    arrayExtend();
+    objectExtend();
+    symbol();
+    setMap();
+    proxy();
+    reflectAlias();
+    promise();
+    iterator();
+    classStudy();
+
 });
--- src/es6/section/class.js
+++ src/es6/section/class.js
@@ -1,16 +1,84 @@
 function classStudy() {
     console.log('class');
 
-    class Add {
-        constructor() {
-            this.x = 1;
-            this.y = 2;
-        }
-        getAdd() {
-            return this.x + this.y;
-        }
-    }
-    let add = new Add();
-    console.log(add.getAdd());
+    // class Add {
+    //     constructor() {
+    //         this.x = 1;
+    //         this.y = 2;
+    //     }
+    //     getAdd() {
+    //         return this.x + this.y;
+    //     }
+    // }
+    // let add = new Add();
+    // console.log(add.getAdd());
+
+    // class Counter {
+    //     constructor(n) {
+    //         this.x = n;
+    //         this.add = this.add.bind(this);
+    //         console.log(Counter.name);
+    //     }
+    //     add() {
+    //         ++this.x;
+    //         console.log(this.x);
+    //     }
+    // }
+    // let counter = new Counter(5);
+    // counter.add();
+
+    // class Foo {
+    //     constructor(...args) {
+    //             this.args = args;
+    //         }
+    //         *[Symbol.iterator]() {
+    //             for (let arg of this.args) {
+    //                 yield arg;
+    //             }
+    //         }
+    // }
+    // for (let x of new Foo('hello', 'world')) {
+    //     console.log(x);
+    // }
+
+    // class Counter {
+    //     constructor() {
+    //         this.x = 2;
+    //         this.add = Counter.add.bind(this);
+    //         console.log(Counter.name);
+    //     }
+    //     // static定义的方法只能使用类调用,不能使用this调用
+    //     static add() {
+    //         console.log(111);
+    //         ++this.x;
+    //         console.log(this.x);
+    //     }
+    // }
+    // Counter.add(); // 没有实例化不存在this
+    // let counter = new Counter(2);
+    // counter.add();
+
+    // class Foo {
+    //     static classMethod() {
+    //         return 'hello';
+    //     }
+    // }
+    // Foo.classMethod() // 'hello'
+    // var foo = new Foo();
+    // foo.classMethod()
+
+    // class Foo {
+    //     static bar() { // static定义的方法中this指向类而不是实例
+    //         this.baz();
+    //     }
+    //     static baz() {
+    //         console.log('hello');
+    //     }
+    //     baz() {
+    //         console.log('world');
+    //     }
+    // }
+    // Foo.bar() // hello
+
 }
 export default classStudy;
